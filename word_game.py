84e1b9108250ae3c38425647514ab67bcfa57b7b
James Lee <jamesl33info@gmail.com>
2019-05-15 18:23:48 +0100

    Add a LICENSE and a README

--- word_game.py
+++ word_game.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
-""" Countdown word game solver in Python """
+"""
+This file is part of pycountdown.
 
+Copyright (C) 2019, James Lee <jamesl33info@gmail.com>.
 
-import sys
-import math
-import pickle
-import os.path
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import argparse
 import itertools
+import math
+import os.path
+import pickle
+import sys
 import urllib.error
 import urllib.request
 
@@ -149,5 +166,4 @@ def main():
 
 
 if __name__ == '__main__':
-    import argparse
     main()
